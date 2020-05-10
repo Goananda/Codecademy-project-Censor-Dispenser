d60aa4848d571ca79f6d024e5b336be2b46eb53c
Goananda <neuroananda@protonmail.com>
2020-05-10 09:27:22 +0300

    Code style: maximum line length is 100

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,5 +1,6 @@
-def censor_text(text, phrases, limit=0, del_near=0, letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'"):
-  
+def censor_text(text, phrases, limit=0, del_near=0,
+                letters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'"):
+
   # Words and word positions
   words = []
   word_positions = []
@@ -12,7 +13,7 @@ def censor_text(text, phrases, limit=0, del_near=0, letters="ABCDEFGHIJKLMNOPQRS
     point = [i for i in noletter_inds if i > first_letter][0]
     word_positions.append(first_letter)
     words.append(text[first_letter:point].lower())
-  
+
   # Censor words
   censor_words = set()
   phrases_lists = [phrase.split() for phrase in phrases]
@@ -20,17 +21,17 @@ def censor_text(text, phrases, limit=0, del_near=0, letters="ABCDEFGHIJKLMNOPQRS
   for num_position in range(len(words)):
     for phrase_list in phrases_lists:
       if words[num_position:num_position+len(phrase_list)] == phrase_list:
-        if check >= limit:          
-          censor_words.update(range(num_position - del_near, num_position + len(phrase_list) + del_near))
+        if check >= limit:
+          censor_words.update(range(num_position-del_near, num_position+len(phrase_list)+del_near))
         check += 1
         break
-  
+
   # Censoring full text
   for num_position in [i for i in censor_words if 0 <= i < len(words)]:
     first_letter = word_positions[num_position]
     length = len(words[num_position])
     text = text[:first_letter]+"X"*length+text[first_letter+length:]
-  
+
   return text[:-1]
 
 email_one = open("email_one.txt", "r").read()
@@ -38,8 +39,12 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "learning algorithms", "her", "herself"]
-negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressing", "concerning", "horrible", "horribly", "questionable"]
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation",
+                     "learning algorithm", "learning algorithms", "her", "herself"]
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed",
+                  "out of control", "help", "unhappy", "bad", "upset", "awful", "broken",
+                  "damage", "damaging", "dismal", "distressed", "distressing", "concerning",
+                  "horrible", "horribly", "questionable"]
 
 email_one_censored = censor_text(email_one, ["learning algorithms"])
 email_two_censored = censor_text(email_two, proprietary_terms)
